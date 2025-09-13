cdbe7cbc89b072ddfc84cc3ce0beb3c1d40fb934
Jimmy Rozo <jimmy.rozo@datamart.co>
2025-09-12 22:16:09 -0500

    fix: Update header text to "Inmobiliaria XYZ" for improved branding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import PropertyClient from "@/modules/properties/components/PropertyClient/Prope
 export default async function Home() {
   return (
     <div className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl font-semibold text-black">Real estate</h1>
+      <h1 className="text-4xl font-semibold text-blue-950">Inmobiliaria XYZ</h1>
       <div className="w-full h-full px-4 py-6 sm:px-8 sm:py-8 md:px-12 md:py-10 lg:px-20 lg:py-12">
         <PropertyClient />
 
